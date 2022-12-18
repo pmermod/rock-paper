1dabf84e8a050db12c2b20221f3bf897a230b097
pmermod <93401090+pmermod@users.noreply.github.com>
2022-12-18 16:22:28 -0500

    Add run game function

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 (function () {
   const options = ["rock", "paper", "scissors"];
 
-  const getComputerChoice = () => {
-    return options[Math.floor(Math.random() * options.length)];
-  };
-
   const buttons = document.querySelectorAll("button");
   buttons.forEach((button) => {
-    button.addEventListener("click", playGame);
+    button.addEventListener("click", runGame);
   });
+
+  const runGame = (e) => {
+    const playerSelection = e.target.id;
+    const computerSelection = getComputerSelection();
+    const outcome = getOutcome(playerSelection, computerSelection);
+  };
+
+  // Continue here with displaying the outcome
+
+  const getComputerSelection = () => {
+    return options[Math.floor(Math.random() * options.length)];
+  };
 })();
