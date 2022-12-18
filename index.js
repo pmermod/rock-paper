2c28bb76afbcb4b716e8dda0b4fcc16e336cebd5
pmermod <93401090+pmermod@users.noreply.github.com>
2022-12-18 16:28:18 -0500

    Add comments for run game function

--- index.js
+++ index.js
@@ -6,13 +6,14 @@
     button.addEventListener("click", runGame);
   });
 
+  // This function defines the computer selection and player selection for the function that will determine who is the winner. It sets the result of the future getOutcome function equal to outcome
   const runGame = (e) => {
     const playerSelection = e.target.id;
     const computerSelection = getComputerSelection();
     const outcome = getOutcome(playerSelection, computerSelection);
   };
 
-  // Continue here with displaying the outcome
+  // Continue here with displaying the outcome and define getOutcome function
 
   const getComputerSelection = () => {
     return options[Math.floor(Math.random() * options.length)];
